34ea4f5d922ed9225d3458e5fa42563ede196ea9
leon nwosu <nwosuleon@gmail.com>
2025-05-22 03:23:17 -0500

    change

--- app/api_client.py
+++ app/api_client.py
@@ -4,8 +4,48 @@ import json
 
 API_KEY = os.getenv("FOOTBALL_API_KEY")
 
+def get_id(player_name):
+
+    if '%20' in player_name:
+        first_name = player_name.split('%20')[0]
+        last_name = player_name.split('%20')[-1]
+    else:
+        last_name = player_name
+
+    url = f"https://v3.football.api-sports.io/players/profiles?search={last_name}"
+    headers = {
+        "x-apisports-key": "0675e46ccc9a2d82a91c49a933e4d6da"
+        # "x-apisports-key": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if data is None:
+            return None
+        else :
+            return data  # We’ll extract specific info later
+    
+    else:
+        print(f"API error: {response.status_code}")
+        return None
+
 def get_player_data(player_name):
-    url = f"https://v3.football.api-sports.io/players?search={player_name}&season=2023"
+
+    
+    
+
+    # Get player ID, league ID, and team ID
+    dataprevious = get_id(player_name)
+
+    if dataprevious is None:
+        return None
+    
+    player_id = dataprevious['response'][0]['player']['id']
+    
+
+
+    url = f"https://v3.football.api-sports.io/players?id={player_id}&season=2023"
     headers = {
         "x-apisports-key": "0675e46ccc9a2d82a91c49a933e4d6da"
     }
@@ -13,9 +53,15 @@ def get_player_data(player_name):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        print("API Response: ")
-        print(data)
-        return data  # We’ll extract specific info later
+
+        if data is None:
+            print("No data found for the player.")
+            return None
+        else :
+            print("API Response: ")
+            print(data)
+            return data  # We’ll extract specific info later
+    
     else:
         print(f"API error: {response.status_code}")
         return None
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from app import app
 from app.api_client import get_player_data
+from urllib.parse import quote_plus
 
 
 @app.route("/")
@@ -10,7 +11,17 @@ def index():
 @app.route("/player")
 def player():
     name = request.args.get("name")
-    player_data = get_player_data(name)
+    if not name:
+        return "Player name is required", 400
+
+    # handle special characters in the player name
+    player_name = quote_plus(name)
+
+    # Map league names to their respective IDs
+   
+    
+    player_data = get_player_data(player_name)
+
 
     
     return render_template("player.html", player_name=name)
