b1df908c1c590eb7ef363abbe58a9c47d003774f
leon nwosu <nwosuleon@gmail.com>
2025-05-22 13:25:09 -0500

    returns the player data

--- app/api_client.py
+++ app/api_client.py
@@ -1,56 +1,63 @@
 import requests
 import os 
 import json
+from urllib.parse import quote_plus as qoute_plus
 
 API_KEY = os.getenv("FOOTBALL_API_KEY")
 
-def get_id(player_name):
 
-    if '%20' in player_name:
-        first_name = player_name.split('%20')[0]
-        last_name = player_name.split('%20')[-1]
+""" the get id function is not used in the current code, but it can be used to get the player id from the player name
+    I decieded to just use the player name and league id but if you want to change it to only player name we have to use the id function
+    then have another function to get the league id from the player id then add it to the the get palyer data function"""
+
+# def get_id(player_name):
+
+#     if '%20' in player_name:
+#         first_name = player_name.split('%20')[0]
+#         last_name = player_name.split('%20')[-1]
+#     else:
+#         last_name = player_name
+
+#     url = f"https://v3.football.api-sports.io/players/profiles?search={last_name}"
+#     headers = {
+#         "x-apisports-key": "0675e46ccc9a2d82a91c49a933e4d6da"
+#         # "x-apisports-key": API_KEY
+#     }
+
+#     response = requests.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         if data is None:
+#             return None
+#         else :
+#             return data  # We’ll extract specific info later
+    
+#     else:
+#         print(f"API error: {response.status_code}")
+#         return None
+
+def get_player_data(player_name,league_id):
+    
+
+    season = 2023
+    
+    #handle spaces in name
+    if ' ' in player_name:
+        first_name = player_name.split(' ')[0]
+        last_name = player_name.split(' ')[-1]
     else:
         last_name = player_name
 
-    url = f"https://v3.football.api-sports.io/players/profiles?search={last_name}"
-    headers = {
-        "x-apisports-key": "0675e46ccc9a2d82a91c49a933e4d6da"
-        # "x-apisports-key": API_KEY
-    }
+    #handle special characters in name
+    encoded_name = qoute_plus(last_name.strip())
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if data is None:
-            return None
-        else :
-            return data  # We’ll extract specific info later
-    
-    else:
-        print(f"API error: {response.status_code}")
-        return None
-
-def get_player_data(player_name):
-
-    
-    
-
-    # Get player ID, league ID, and team ID
-    dataprevious = get_id(player_name)
-
-    if dataprevious is None:
-        return None
-    
-    player_id = dataprevious['response'][0]['player']['id']
-    
-
-
-    url = f"https://v3.football.api-sports.io/players?id={player_id}&season=2023"
+    url = f"https://v3.football.api-sports.io/players?search={encoded_name}&league={league_id}&season={season}"
     headers = {
         "x-apisports-key": "0675e46ccc9a2d82a91c49a933e4d6da"
     }
 
     response = requests.get(url, headers=headers)
+    print(f"API URL: {url}")
     if response.status_code == 200:
         data = response.json()
 
@@ -60,6 +67,18 @@ def get_player_data(player_name):
         else :
             print("API Response: ")
             print(data)
+
+            # I still need to handle special characters in the name
+            # Check if there is more than one player
+            
+            # if data.get('response') and len(data['response']) > 0:
+            #    for player in data['response']:
+            #         if player['player']['firstname'].strip().lower() == first_name.strip().lower() and player['player']['lastname'].strip().lower() == last_name.strip().lower():
+            #             # If the player is found, return the player data
+            #             print("Player found: ")
+            #             print(player)
+            #             return player
+
             return data  # We’ll extract specific info later
     
     else:
--- app/routes.py
+++ app/routes.py
@@ -11,16 +11,29 @@ def index():
 @app.route("/player")
 def player():
     name = request.args.get("name")
+    league = request.args.get("league")
     if not name:
         return "Player name is required", 400
+    if not league:
+        return "League is required", 400
 
     # handle special characters in the player name
-    player_name = quote_plus(name)
+    player_name = name.strip()
 
     # Map league names to their respective IDs
+    if league == "Premier League":
+        league_id = 39
+    elif league == "La Liga":  
+        league_id = 140
+    elif league == "Serie A":
+        league_id = 135
+    elif league == "Bundesliga":
+        league_id = 78
+    elif league == "Ligue 1":
+        league_id = 61
    
     
-    player_data = get_player_data(player_name)
+    player_data = get_player_data(player_name, league_id)
 
 
     
